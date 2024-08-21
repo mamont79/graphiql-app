6955bcd3883e1da6b56fd6b142ed474811e72fd0
Pavel Dalhou <dalhoupavel@gmail.com>
2024-08-21 22:16:47 +0200

    feat: add rss logo to footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export const Footer = () => {
   const year = new Date().getFullYear();
 
@@ -5,7 +7,22 @@ export const Footer = () => {
     <header className="flex flex-row justify-around bg-[#1da1f2] w-full h-[80px]">
       <div className="content-center">About team</div>
       <div className="content-center">{year}</div>
-      <div className="content-center">Link to course</div>
+      <div className="content-center">
+        <a
+          className="flex place-items-center"
+          href="https://rs.school/courses/reactjs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/rsschool-logo.svg"
+            alt="RSS Logo"
+            className="dark:invert"
+            width={100}
+            height={24}
+          />
+        </a>
+      </div>
     </header>
   );
 };
